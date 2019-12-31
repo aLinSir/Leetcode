9d74d97bd0f25175f3ecf05e59efe684fe6424f8
aLinSir <377717816@qq.com>
2019-12-31 10:53:38 +0800

    2019-12-31

--- easy/645.错误的集合.py
+++ easy/645.错误的集合.py
@@ -4,11 +4,14 @@ from typing import List
 
 
 def findErrorNums(nums: List[int]) -> List[int]:
-    
+    l = [n for n in nums if nums.count(n) == 2]
+    for i in range(1, len(nums) + 1):
+        if i not in nums:
+            l[1] = i
 
-    return re
+    return l
 
 
-nums = [2,2]
+nums = [1, 2, 2, 4]
 y = findErrorNums(nums)
 print(y)
